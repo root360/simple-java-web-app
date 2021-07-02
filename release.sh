5666b4c9f5bceda599ceb3f944799eebd6f2d272
Andreas Ulm <andreas.ulm@root360.de>
2021-07-02 14:05:06 +0200

    improved run.sh & release.sh
    Signed-off-by: Andreas Ulm <andreas.ulm@root360.de>

--- release.sh
+++ release.sh
@@ -3,21 +3,28 @@
 set -e
 
 SCRIPTDIR="$(dirname "$(readlink -f "$0")")"
+version="${1:-1.0.0-SNAPSHOT}"
+
+echo "Building package with version '${version}'"
 
 cat > /tmp/build.sh <<EOF
 #!/bin/bash
 export DEBIAN_FRONTEND="noninteractive"
-apt update -qq -y
-apt install -qq -y maven
+apt update -qq -y >/dev/null
+apt install -qq -y maven >/dev/null
 
 cd /app
+sed -i "s/1.0.0-SNAPSHOT/${version}/" pom.xml
 mvn clean package
+git checkout pom.xml
 chown -R "${UID}:${UID}" ./
 EOF
 
 chmod a+x /tmp/build.sh
 
 rm -rf "${SCRIPTDIR}/target"
+
+git tag --annotate "${version}" -s -m "release of ${version}"
 
 docker run -ti --rm -v "${SCRIPTDIR}:/app" -v "/tmp/build.sh:/build.sh" ubuntu:focal /build.sh
 
--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
-cd ./target && java -jar root360-simple-tomcat-jar-1.0.0-SNAPSHOT.jar
+#!/bin/bash
+
+SCRIPTDIR="$(dirname "$(readlink -f "$0")")"
+
+mapfile -t jars < <(find "${SCRIPTDIR}" -name 'root360-*.jar')
+
+if [[ "${#jars}" -eq 1 ]]; then
+    jar="${jars[0]}"
+else
+    select jar in "${jars[@]}"; do
+        break
+    done
+fi
+
+java -jar "${jar}"
